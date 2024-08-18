3bc563b20d10cdeb0e28ff126cfe2cded6f95060
lomtech <lomaliimadaev@gmail.com>
2024-08-18 18:20:42 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // JavaScript, um das Calendly-Widget beim Klicken des Buttons anzuzeigen
 document.getElementById('showCalendly').addEventListener('click', function () {
-  document.getElementById('calendlyWidgetContainer').style.display = 'block';
-  document.getElementById('calendlyWidget').style.display = 'block';
-  this.style.display = 'none'; // Versteckt den Button nach dem Klicken
-});
+    document.getElementById('calendlyWidgetContainer').style.display = 'block';
+    this.style.display = 'none'; // Versteckt den Button nach dem Klicken
+  });
+  
